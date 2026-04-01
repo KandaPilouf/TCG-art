154a442440ae75eee5f6dd9521525a09539b44ce
KandaPilouf <samikhan199758@gmail.com>
2026-04-01 10:03:23 +0200

    organized files and added nav from index

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php include './component/head.html' ?>
-    <main>
-      <?php
-        $page = $_GET['categorie'];
-        include $page . '.php';
-      ?>
-    </main>
+<main>
+  <?php
+
+  if (empty($_GET)) {
+    $page = 'home';
+  } elseif (isset($_GET['categorie'])) {
+
+    $page = $_GET['categorie'];
+  } elseif (isset($_GET['item'])) {
+
+    $page = $_GET['item'];
+  }
+
+  include $page . '.php';
+  ?>
+</main>
 <?php include './component/foot.php' ?>
