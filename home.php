bc06d70cb79344e0f52cf1fc4f807056c37b7144
KandaPilouf <samikhan199758@gmail.com>
2026-04-03 14:35:28 +0200

    added searchbar in home html

--- home.php
+++ home.php
@@ -4,4 +4,8 @@
     <div class="home_card">
         <img src="./assets/img/chocobo.png" alt="card image">
     </div>
+    <div id="home_searchbar">
+        <input type="text" name="searchbar">
+        <button>search</button>
+    </div>
 </main>
