6b050f36709c95644c8f98da910b88d6b55e7611
KandaPilouf <samikhan199758@gmail.com>
2026-04-10 16:25:22 +0200

    first draft html catalogue

--- catalogue.php
+++ catalogue.php
@@ -1,3 +1,13 @@
 <main>
-    <h1>Catalogue</h1>
+    <input type="text" name="searchbar">
+    <select name="tags"></select>
+    <select name="style"></select>
+    <select name="universe" ></select>
+    <figure>
+        <img src="./assets/img/chocobo.png" alt="card image">
+        <figcaption>
+            card data/tags/style/etc
+        </figcaption>
+        <button>add to deck</button>
+    </figure>
 </main>
