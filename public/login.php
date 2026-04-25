dc179167dad356d11b88da1cb538d9fdea5f9b5a
KandaPilouf <samikhan199758@gmail.com>
2026-04-25 11:31:27 +0200

    creation basic login page

---
+++ public/login.php
@@ -0,0 +1,13 @@
+<main>
+    <form method="POST" action="connect.php">
+        <label for="username">Nom d'utilisateur</label>
+        <input id="username" name="username" value="" required>
+
+        <label for="password">Mot de passe</label>
+        <input id="password" name="password" value="" type="password" required>
+
+        <button type="submit">Se connecter</button>
+    </form>
+
+    <a href="register.php">Inscription</a><form action=""></form>
+</main>
