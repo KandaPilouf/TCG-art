e2ba33265ca302094bafbe65faa7acd5ed317fdf
KandaPilouf <samikhan199758@gmail.com>
2026-05-08 14:44:09 +0200

    added router.php and transfered code from index.php

--- index.php
+++ index.php
@@ -1,59 +1,5 @@
 <?php
-
-var_dump($_SERVER['REQUEST_URI']);
-/* current url assigned */
-$current_url = $_SERVER['REQUEST_URI'];
-
-/* cleanup of url */
-$current_url = trim($current_url, '/');
-$current_url = str_replace('//', '/', $current_url);
-
-/* default values */
-$segments = [];
-$controller = 'home';
-$action = 'index';
-$id = null;
-$is_admin = false;
-
-/* explode into segments*/
-if (str_contains($current_url, '/')) {
-  $segments = explode('/', $current_url);
-} 
-
-/* check if admin zone and shift url segments*/
-if (isset($segments[0]) && $segments[0] === 'admin') {
-  array_shift($segments);
-  $is_admin = true;
-}
-
-
-/* set every segments */
-if (isset($segments[0])) {
-  $controller = $segments[0];
-}
-if (isset($segments[1])) {
-  $action = $segments[1];
-}
-if (isset($segments[2]) && is_numeric($segments[2])) {
-  $id = $segments[2];
-}
-
-/* sets file */
-$file = $controller . '.php';
-$page = 'home';
-
-/* check if file exists */
-if(file_exists("./public/".$file)){
-  $page = $controller;
-}else{
-  $page = 'home';
-}
-
-echo "URL : " . $_SERVER['REQUEST_URI'] . PHP_EOL . PHP_EOL;
-echo "Controller : " . $controller . ".php" . PHP_EOL;
-echo "Action : " . $action . "()" . PHP_EOL;
-echo "Identifiant : '" . $id . "'" . PHP_EOL;
-echo ($is_admin) ? 'Admin : yes' : 'Admin : no';
+require 'router.php';
 
 /* get page with href when icons are clicked */
  if (isset($_GET['categorie'])) {
@@ -62,12 +8,12 @@ echo ($is_admin) ? 'Admin : yes' : 'Admin : no';
    $page = $_GET['item'];
  }
 
-include './component/head.php';
-include './database/database.php';
+require './component/head.php';
+require './database/database.php';
 ?>
 <main>
   <?php
-  include './public/' . $page . '.php';
+  require './public/' . $page . '.php';
   ?>
 </main>
-<?php include './component/foot.php' ?>
+<?php require './component/foot.php' ?>
