630ffd85dff21d9288d43ff908c7ce24682b55a7
KandaPilouf <samikhan199758@gmail.com>
2026-05-23 09:08:30 +0200

    access to pdo from index

--- index.php
+++ index.php
@@ -3,13 +3,14 @@
 require 'core/http.php';
 require 'core/router.php';
 require 'core/html.php';
+require './database/database.php';
 
 $base = __DIR__.'/app';
 
 $segments = http_in($_SERVER['REQUEST_URI']);
 $route = route($segments);
 
-$main = run($route, $base);
+$main = run($route, $base, $pdo);
 $body = render('app/views/_layout.php', ['page_content' => $main]);
 http_out(200, $body);
 
