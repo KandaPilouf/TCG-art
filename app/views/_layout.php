10eb6c134bb85ce37a6a82bc5ee1ac447b2fb188
KandaPilouf <samikhan199758@gmail.com>
2026-05-23 19:19:28 +0200

    added font.css

--- app/views/_layout.php
+++ app/views/_layout.php
@@ -4,9 +4,10 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="/styles/css/common.css">
     <link rel="stylesheet" href="/styles/css/colors.css">
+    <link rel="stylesheet" href="/styles/css/fonts.css">
     <link rel="stylesheet" href="/styles/css/typo.css">
+    <link rel="stylesheet" href="/styles/css/common.css">
 
     <?php
     $page_css = __DIR__ . '/../../styles/css/' . $entity . '.css';
