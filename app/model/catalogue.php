0a0a9acd6d8b6e96d95626edb1eb99aec0dc2f0a
KandaPilouf <samikhan199758@gmail.com>
2026-05-23 11:39:17 +0200

    change sql query to get all foreign key reference

--- app/model/catalogue.php
+++ app/model/catalogue.php
@@ -7,7 +7,18 @@ function get_all_items($pdo){
 }
 
 function get_one_items($pdo, $slug){
-    $sql = "SELECT * FROM card WHERE slug = ?";
+    $sql = "SELECT card.*,
+       style.style,
+       universe.universe,
+       variant.variant,
+       color.color
+FROM card
+JOIN style    ON style.id    = card.style_id
+JOIN universe ON universe.id = card.universe_id
+JOIN variant  ON variant.id  = card.variant_id
+JOIN color    ON color.id    = card.primary_color_id
+WHERE card.slug = ?
+";
     $stmt = $pdo->prepare($sql);
     $stmt->execute([$slug]);
     return $stmt->fetch();
