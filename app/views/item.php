3cfbfe683af9866de0eabf71f219f1fc56952eb6
KandaPilouf <samikhan199758@gmail.com>
2026-05-23 14:07:29 +0200

    first draft for item page view

--- app/views/item.php
+++ app/views/item.php
@@ -1,1 +1,18 @@
-hello
+<figure>
+    <h1><?php echo $card['name']; ?></h1>
+    <img src=<?php echo $card['img']; ?> alt="card image">
+    <figcaption>
+        <ul>
+            <li>Artist: <?php echo $card['artist'] ?></li>
+        </ul>
+        <ul>
+            <li>Style: <?php echo $card['style'] ?></li>
+        </ul>
+        <ul>
+            <li>Universe: <?php echo $card['universe'] ?></li>
+        </ul>
+        <ul>
+            <li>Tag: <?php echo $card['tags'];?></li>
+        </ul>
+    </figcaption>
+</figure>
