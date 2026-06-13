df820545012182076a872436af859c7b90f861dc
KandaPilouf <samikhan199758@gmail.com>
2026-06-13 22:55:23 +0200

    search bar logic

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -4,6 +4,7 @@ require 'app/model/home.php';
 function home_index($pdo){
     $data = [];
     $data['card'] = get_random_card($pdo);
+    $data['cards'] = get_all_cards($pdo);
     return render("app/views/home.php", $data);
 }
 
--- app/model/home.php
+++ app/model/home.php
@@ -7,3 +7,9 @@ function get_random_card($pdo)
     $stmt = $pdo->query($sql);
     return $stmt->fetch();
 }
+
+function get_all_cards($pdo){
+    $sql = "SELECT name FROM card";
+    $stmt = $pdo->query($sql);
+    return $stmt->fetchAll();
+}
