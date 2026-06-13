ef8082d149a6740acf729deafaee931c5f8c21a6
KandaPilouf <samikhan199758@gmail.com>
2026-06-13 21:51:21 +0200

    removed admin verif (already handled by index)

--- app/admin/controllers/home.php
+++ app/admin/controllers/home.php
@@ -1,9 +1,6 @@
 <?php
-require "app/model/admin.php";
-require "core/auth.php";
 
 function home_index($pdo){
-    require_admin();
     $data = [];
     return render("app/admin/views/index.php", $data);
 }
