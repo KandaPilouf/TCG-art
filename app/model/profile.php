ff43715100ba8366728e0923aaadd7e545e9c211
KandaPilouf <samikhan199758@gmail.com>
2026-06-14 10:35:38 +0200

    unecessary function for profile

--- app/model/profile.php
+++ app/model/profile.php
@@ -7,10 +7,3 @@ function get_profile_data($pdo, $id)
     return $stmt->fetch();
 }
 
-
-function add_card($pdo, $name, $slug, $img, $artist)
-{
-    $stmt = $pdo->prepare("INSERT INTO `card` (`id`, `name`, `slug`, `artist`, `img`, `style_id`, `universe_id`, `creation_date`, `variant_id`, `primary_color_id`) VALUES (NULL, ?, ?, ?, ?, NULL, NULL, NULL, NULL, NULL);");
-
-    $stmt->execute([$name, $slug, $artist, $img]);
-}
