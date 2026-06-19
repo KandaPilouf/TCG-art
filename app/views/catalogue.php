c27520d0468777e01c105dc0241c0435156f0562
KandaPilouf <samikhan199758@gmail.com>
2026-06-19 14:28:55 +0200

    catalogue styling

--- app/views/catalogue.php
+++ app/views/catalogue.php
@@ -1,20 +1,30 @@
+<div id="filters">
     <input type="text" name="searchbar">
     <select name="tags"></select>
     <select name="style"></select>
     <select name="universe"></select>
 
+    
+</div>
+
+<div id="cards">
     <?php
     foreach ($cards as $card) {
     ?>
         <figure>
-            <h1><?php echo $card['name']; ?></h1>
-            <img src=<?php echo $card['img']; ?> alt="card image">
-            <figcaption>
-                <span> Artist: <?php echo $card['artist'];?></span>
-            </figcaption>
-            <button><a href="/catalogue/show/<?php echo $card['slug'] ?>">show item</a></button>
+            <div id="card_caption">
+                <img src=<?php echo $card['img']; ?> alt="card image">
+                <figcaption>
+                    <h2><?php echo $card['name']; ?></h2>
+                    <span> Artist: <?php echo $card['artist']; ?></span>
+                </figcaption>
+            </div>
+            <div class="btn-pos">
+                <a class="btn-card" href="/catalogue/show/<?php echo $card['slug'] ?>">show card</a>
+                <a class="btn-card">Add card</a>
+            </div>
         </figure>
-
     <?php
     }
     ?>
+</div>
