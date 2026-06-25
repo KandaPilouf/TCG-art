4366064f60640ad6e5e986c712d0a4c28caec83c
KandaPilouf <samikhan199758@gmail.com>
2026-06-25 21:10:49 +0200

    styling deck lists

--- app/views/decks.php
+++ app/views/decks.php
@@ -1,11 +1,19 @@
-<h1>hello deck page</h1>
-<?php
-foreach ($decks as $deck) { ?>
-    <a href="decks/show/<?= $deck['id'] ?>"><?= $deck['name'] ?></a>
-<?php
-}
-?>
-<form method="POST" action="/decks/add">
-    <input name="new_deck" placeholder="Deck name" required>
-    <button type="submit">Create a collection</button>
-</form>
+<div id="decks-list">
+    <div id="title">
+        <h1>Deck page</h1>
+    </div>
+
+    <form method="POST" action="/decks/add">
+        <input name="new_deck" placeholder="Deck name" required>
+        <button type="submit">Create a collection</button>
+    </form>
+
+    <div id="content">
+        <?php
+        foreach ($decks as $deck) { ?>
+            <a href="/decks/show/<?= $deck['id'] ?>"><?= $deck['name'] ?></a>
+        <?php
+        }
+        ?>
+    </div>
+</div>
