2b316c3dfb3dd3144c3b84e808a52f6e94ed9a28
KandaPilouf <samikhan199758@gmail.com>
2026-06-26 00:16:02 +0200

    default option for decks

--- app/views/item.php
+++ app/views/item.php
@@ -16,6 +16,7 @@
             <input type="hidden" name="redirect" value="/catalogue/show/<?= $card['slug'] ?>">
             <input type="hidden" name="card_id" value="<?= $card['id'] ?>">
             <select name="deck_id" class="btn-add-deck">
+                <option value="">Choose a deck</option>
                 <?php
                 foreach ($user_decks as $deck) { ?>
                     <?php $already = in_array($deck['id'], $decks_with_card) ?>
